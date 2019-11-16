c1bf918176b090b10f410b872196e6fa1a3db52e
Rebecca Parker <rfpparker@gmail.com>
2019-11-16 16:50:59 -0600

    Added tests

--- augur/metrics/commit/test_commit_routes.py
+++ augur/metrics/commit/test_commit_routes.py
@@ -41,4 +41,11 @@ def test_annual_commit_count_ranked_by_repo_in_repo_group_by_group(metrics):
     assert len(data) >= 1
     assert data[0]["net"] > 0
 
+def test_repo_timeline(metrics):
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/20/repo-timeline')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) >= 1
+    assert data[0]["net"] > 0
+
 
